8ed2560a03ac2bb39194e15abdcc12a7f05453f4
wijayy <mertawijaya77@gmail.com>
2025-12-03 09:16:35 +0800

    stash

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,8 @@
-<x-layouts.app.sidebar :title="$title ?? null">
+<x-layouts.app.sidebar :title="$title ?? null" class="">
     <flux:main>
-        {{ $slot }}
+        <flux:session>{{ $title }}</flux:session>
+        <flux:container-sidebar class="">
+            {{ $slot }}
+        </flux:container-sidebar>
     </flux:main>
 </x-layouts.app.sidebar>
