333923e9b3232efa57e35c403cee7ab30246b71f
wijayy <mertawijaya77@gmail.com>
2026-01-15 17:16:01 +0800

    change keyword

--- resources/views/livewire/landingpage/landing-page.blade.php
+++ resources/views/livewire/landingpage/landing-page.blade.php
@@ -36,38 +36,37 @@
             x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 scale-100"
             x-transition:leave-end="opacity-0 scale-90" class="bg-black text-white py-20 overflow-hidden">
             <div class="container mx-auto px-4 max-w-screen-xl md:text-lg mb-10">
-                <div class="font-semibold text-lg md:text-xl mb-4">Keywords</div>
-                <div>Bedugul restaurant</div>
-                <div>Restaurants Bedugul</div>
-                <div>Mediterranean food Bedugul</div>
-                <div>Best restaurants Bedugul</div>
-                <div>Best restaurant Bedugul</div>
-                <div>Steak restaurant Bedugul</div>
-                <div>Mediterranean restaurant Bedugul</div>
-                <div>Fine dining Bedugul</div>
-                <div>Steak restaurants Bedugul</div>
-                <div>Golf course restaurant Bedugul</div>
-                <div>Golf restaurant Bedugul</div>
-                <div>Mediterranean restaurants Bedugul</div>
-                <div>Best steak restaurant Bedugul</div>
-                <div>Golf course with restaurant Bedugul</div>
-                <div>Golf courses with restaurants Bedugul</div>
-                <div>Golf restaurants Bedugul</div>
-                <div>Bedugul restaurants</div>
-                <div>Bedugul mediterranean food</div>
-                <div>Bedugul best restaurants</div>
-                <div>Bedugul best restaurant</div>
-                <div>Bedugul steak restaurant</div>
-                <div>Bedugul mediterranean restaurant</div>
-                <div>Bedugul steak restaurants</div>
-                <div>Bedugul fine dining</div>
-                <div>Bedugul golf course restaurant</div>
-                <div>Bedugul golf restaurant</div>
-                <div>Bedugul mediterranean restaurants</div>
-                <div>Bedugul best steak restaurant</div>
-                <div>Bedugul golf course with restaurant</div>
-                <div>Bedugul golf courses with restaurants</div>
-                <div>Bedugul golf restaurants</div>
+                <h3>Bedugul restaurant</h3>
+                <h3>Restaurants Bedugul</h3>
+                <h3>Mediterranean food Bedugul</h3>
+                <h3>Best restaurants Bedugul</h3>
+                <h3>Best restaurant Bedugul</h3>
+                <h3>Steak restaurant Bedugul</h3>
+                <h3>Mediterranean restaurant Bedugul</h3>
+                <h3>Fine dining Bedugul</h3>
+                <h3>Steak restaurants Bedugul</h3>
+                <h3>Golf course restaurant Bedugul</h3>
+                <h3>Golf restaurant Bedugul</h3>
+                <h3>Mediterranean restaurants Bedugul</h3>
+                <h3>Best steak restaurant Bedugul</h3>
+                <h3>Golf course with restaurant Bedugul</h3>
+                <h3>Golf courses with restaurants Bedugul</h3>
+                <h3>Golf restaurants Bedugul</h3>
+                <h3>Bedugul restaurants</h3>
+                <h3>Bedugul mediterranean food</h3>
+                <h3>Bedugul best restaurants</h3>
+                <h3>Bedugul best restaurant</h3>
+                <h3>Bedugul steak restaurant</h3>
+                <h3>Bedugul mediterranean restaurant</h3>
+                <h3>Bedugul steak restaurants</h3>
+                <h3>Bedugul fine dining</h3>
+                <h3>Bedugul golf course restaurant</h3>
+                <h3>Bedugul golf restaurant</h3>
+                <h3>Bedugul mediterranean restaurants</h3>
+                <h3>Bedugul best steak restaurant</h3>
+                <h3>Bedugul golf course with restaurant</h3>
+                <h3>Bedugul golf courses with restaurants</h3>
+                <h3>Bedugul golf restaurants</h3>
             </div>
         </div>
 
