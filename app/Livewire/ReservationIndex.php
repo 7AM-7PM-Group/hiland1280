e1e2106f596939a66805ef8f4f8e12af8d39fad3
wijayy <mertawijaya77@gmail.com>
2026-05-05 16:21:44 +0800

    change how to get reservation data

--- app/Livewire/ReservationIndex.php
+++ app/Livewire/ReservationIndex.php
@@ -31,7 +31,7 @@ class ReservationIndex extends Component
         if ($this->date) {
             $this->reservations = $this->reservations->whereDate('reservation_time', $this->date);
         }
-        $this->reservations = $this->reservations->order('reservation_time')->get();
+        $this->reservations = $this->reservations->latest()->get();
     }
 
     public function openNoteModal($id)
