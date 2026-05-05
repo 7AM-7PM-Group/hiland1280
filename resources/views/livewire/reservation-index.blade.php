471356636090cee31cc3155e1841ab1e7c28b9c8
wijayy <mertawijaya77@gmail.com>
2026-05-05 16:29:12 +0800

    change how to get reservation data

--- resources/views/livewire/reservation-index.blade.php
+++ resources/views/livewire/reservation-index.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="font-semibold">Summary</div>
     </div>
-    <div class="flex mt-4 py-2 font-semibold gap-4">
+    <div class="flex mt-4 py-2 font-semibold min-w-3xl gap-4">
         <div class="w-10">#</div>
         <div class="w-1/5">Reservator</div>
         <div class="w-1/5 text-center">Time</div>
@@ -15,7 +15,7 @@
     </div>
 
     @foreach ($reservations as $item)
-        <div class="flex mt-4 items-center gap-4">
+        <div class="flex mt-4 items-center min-w-3xl gap-4">
             <div class="w-10">#</div>
             <div class="w-1/5 flex flex-col">
                 <div class="">{{ $item->name }}</div>
