3e416dc30d7299f3bc564aca27996e82808dd7d3
wijayy <mertawijaya77@gmail.com>
2026-05-05 16:31:30 +0800

    change how to get reservation data

--- resources/views/livewire/reservation-index.blade.php
+++ resources/views/livewire/reservation-index.blade.php
@@ -5,33 +5,36 @@
         </div>
         <div class="font-semibold">Summary</div>
     </div>
-    <div class="flex mt-4 py-2 font-semibold overflow-x-auto min-w-3xl gap-4">
-        <div class="w-10">#</div>
-        <div class="w-1/5">Reservator</div>
-        <div class="w-1/5 text-center">Time</div>
-        <div class="w-1/5 text-center">Number of guest</div>
-        <div class="w-1/5 text-center">Occasion</div>
-        <div class="w-1/5 text-center">Note</div>
-    </div>
+    <div class="overflow-x-auto">
 
-    @foreach ($reservations as $item)
-        <div class="flex mt-4 items-center overflow-x-auto min-w-3xl gap-4">
+        <div class="flex mt-4 py-2 font-semibold min-w-3xl gap-4">
             <div class="w-10">#</div>
-            <div class="w-1/5 flex flex-col">
-                <div class="">{{ $item->name }}</div>
-                <a href="https://wa.me/{{ $item->phone }}" class="">{{ $item->phone }}</a>
-            </div>
-            <div class="w-1/5 text-center">{{ $item->reservation_time->format('F d, Y H:i') }}</div>
-            <div class="w-1/5 text-center">{{ $item->number_of_guests }} Pax</div>
-            <div class="w-1/5 text-center">{{ $item->occasion }}</div>
-            <div class="w-1/5 text-center">
-                @if ($item->note)
-                    <flux:button size="sm" variant="primary" color="sky" icon="eye"
-                        wire:click='openNoteModal({{ $item->id }})'></flux:button>
-                @endif
-            </div>
+            <div class="w-1/5">Reservator</div>
+            <div class="w-1/5 text-center">Time</div>
+            <div class="w-1/5 text-center">Number of guest</div>
+            <div class="w-1/5 text-center">Occasion</div>
+            <div class="w-1/5 text-center">Note</div>
         </div>
-    @endforeach
+
+        @foreach ($reservations as $item)
+            <div class="flex mt-4 items-center min-w-3xl gap-4">
+                <div class="w-10">#</div>
+                <div class="w-1/5 flex flex-col">
+                    <div class="">{{ $item->name }}</div>
+                    <a href="https://wa.me/{{ $item->phone }}" class="">{{ $item->phone }}</a>
+                </div>
+                <div class="w-1/5 text-center">{{ $item->reservation_time->format('F d, Y H:i') }}</div>
+                <div class="w-1/5 text-center">{{ $item->number_of_guests }} Pax</div>
+                <div class="w-1/5 text-center">{{ $item->occasion }}</div>
+                <div class="w-1/5 text-center">
+                    @if ($item->note)
+                        <flux:button size="sm" variant="primary" color="sky" icon="eye"
+                            wire:click='openNoteModal({{ $item->id }})'></flux:button>
+                    @endif
+                </div>
+            </div>
+        @endforeach
+    </div>
     <flux:modal name="noteModal">
         <div class="mt-4">{{ $note }}</div>
     </flux:modal>
